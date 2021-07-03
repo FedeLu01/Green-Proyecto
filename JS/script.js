f4508cf318d05ced1c3fcd38ae00764dd2a55e24
FedeLu01 <fedelucero95@gmail.com>
2021-07-03 15:07:16 -0300

    js

--- JS/script.js
+++ JS/script.js
@@ -1,29 +1,10 @@
-const packs = [{pack: 1, precio: 230}, {pack: 2, precio: 330}, {pack: 3, precio: 540}, {pack: 4, precio: 720}];
-const sumarEnvio = packs.map(precio => precio.precio += 250);
+let btn = document.getElementById("btn1");
 
-let pedido = parseInt(prompt("Ingrese que pack quiere comprar!: "));
-switch(pedido){
-    case 1:
-        alert("Elegiste el pack x 4 hamburguesas($230)!");
-        alert("El total de tu pedido (con envío) es de $" + sumarEnvio[0]);
-        break;
+btn.addEventListener("click", respuesta);
 
-    case 2: 
-        alert("Elegiste el pack x 6 hamburguesas ($330)!");
-        alert("El total de tu pedido (con envío) es de $" + sumarEnvio[1]);
-        break;
-    case 3: 
-        alert("Elegiste el pack x 10 hamburguesas ($540)!");
-        alert("El total de tu pedido (con envío) es de $" + sumarEnvio[2]);
-        break;
-    case 4: 
-        alert("Elegiste el pack x 15 hamburguesas ($720)!");
-        alert("El total de tu pedido (con envío) es de $" + sumarEnvio[3]);
-        break;    
-    default: 
-        alert("Ingrese un numero de pack válido.");   
+function respuesta(){
+    alert("Gracias por tu compra!");
 }
-
 
 
 
