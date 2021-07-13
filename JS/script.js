b21982f1940213b1a830780a6f077778535b7673
FedeLu01 <fedelucero95@gmail.com>
2021-07-13 14:19:18 -0300

    sin cambios

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,11 @@
-let btn = document.getElementById("btn1");
+let btnCarrito = document.getElementById("carritoCompras");
+let btn = document.getElementsByClassName("compraPack");
+let productos = [{pack: 1, precio: 230}, {pack: 2, precio: 330}, {pack: 3, precio: 540}, {pack: 4, precio: 720}]
 
-btn.addEventListener("click", respuesta);
 
-function respuesta(){
-    alert("Gracias por tu compra!");
-}
+
+
+
 
 
 
