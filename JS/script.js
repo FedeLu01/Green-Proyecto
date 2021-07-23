a88e34f7f0e24dfab757ae123ba8ea3900fc88c7
FedeLu01 <fedelucero95@gmail.com>
2021-07-23 11:31:14 -0300

    changes

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,3 @@
-let btnCarrito = document.getElementById("carritoCompras");
-let btn = document.getElementsByClassName("compraPack");
-let productos = [{pack: 1, precio: 230}, {pack: 2, precio: 330}, {pack: 3, precio: 540}, {pack: 4, precio: 720}]
 
 
 
